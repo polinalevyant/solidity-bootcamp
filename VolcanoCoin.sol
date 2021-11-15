1110df71b46309c1dd8b60feafd7ef538418ad24
polinalevyant <polinalevyant@gmail.com>
2021-11-15 21:33:56 +0000

    hw 5

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -1,58 +1,29 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin {
-    
-    uint total_supply = 10000;
-    address owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    struct Payment {
+contract VolcanoCoin is ERC20, Ownable {
+    
+    constructor() ERC20("VolcanoCoin", "VLC") {
+        _mint(msg.sender, 10000);
+    }
+    
+     struct Payment {
+        address sender;
         address recipient;
         uint amount;
     }
     
-    mapping (address => uint) private balances;
-    mapping (address => Payment[]) private payments;
-    
-    constructor() {
-        owner = msg.sender;
-        balances[owner] = total_supply;
-    }
-    
-    modifier onlyOwner {
-        if (msg.sender == owner) {
-            _;
-        }
-    }
+    mapping (address => Payment[]) public payments;
     
     event SupplyIncreased(uint);
-    event TransactionSuccess(uint, address);
-    
-    function balanceOf(address account) public view returns (uint) {
-        return balances[account];
-    }
-    
-    function getSupply() public view returns (uint) {
-        return total_supply;
-    }
     
     function incraseTotalSupply() public onlyOwner {
-        total_supply += 1000;
-        balances[owner] = total_supply;
-        emit SupplyIncreased(total_supply);
-    }
-    
-    function transfer(uint _amount, address _recipient) public returns (bool) {
-        if (_amount <= balances[msg.sender]) {
-            balances[msg.sender] = balances[msg.sender] -= _amount;
-            balances[_recipient] = balances[_recipient] += _amount;
-            payments[msg.sender].push(Payment({recipient: _recipient, amount: _amount}));
-            emit TransactionSuccess(_amount, _recipient);
-            return true;
-        } else {
-            return false;
-        }
+        _mint(msg.sender, 1000);
+        emit SupplyIncreased(totalSupply());
     }
     
     function getPaymentsCount(address account) public view returns (uint) {
@@ -61,5 +32,20 @@ contract VolcanoCoin {
     
     function getPayments(address account) public view returns (Payment[] memory) {
         return payments[account];
+    }
+    
+    function recordPayment(address _sender, address _recipient, uint _amount) private returns (bool) {
+        payments[msg.sender].push(
+            Payment({
+                sender: _sender, 
+                recipient: _recipient, 
+                amount: _amount}));
+        return true;
+    }
+    
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        recordPayment(msg.sender, recipient, amount);
+        super.transfer(recipient,amount);
+        return true;
     }
 }
